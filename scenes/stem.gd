915290ca5e12782ee3f6950a6e06c620654f0aea
2m80 <138023185+2m80@users.noreply.github.com>
2025-03-28 21:52:59 +0200

    idot

---
+++ scenes/stem.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+var max_branches = 5 
+var branch_sprites = [] 
+
+var branch_pos = [
+	"res://assets/plant/PROC_generic_stem_a.png"
+]
+
+func _ready():
+	grow_branch(Vector2(0, -50), 1)
+
+func grow_branch(start_position, depth):
+	if depth > max_branches:
+		return 
+	var branch = Sprite2D.new()
+	var type = branch_pos[randi_range(0,branch_pos.size()-1)]
+	branch.texture = load(type)
+	branch.offset = Vector2(0, -branch.texture.get_size().y)
+	var size = Image.load_from_file(type).get_size()[1]
+	
+	branch.position = start_position
+	branch.rotation_degrees = randf_range(-30, 30)
+	add_child(branch)
+	branch_sprites.append(branch)
+
+	await get_tree().create_timer(0.5).timeout
+	var new_position = branch.position + Vector2(0, -size).rotated(branch.rotation)
+	grow_branch(new_position, depth + 1)
